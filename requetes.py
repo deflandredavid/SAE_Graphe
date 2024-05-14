4270054daf3291152f96cadfc328007a255eb832
Lobjois <matheo.lobjois@etu.univ-orleans.fr>
2024-05-14 13:49:23 +0200

    Correction requete 4 et création de la requete 5

--- requetes.py
+++ requetes.py
@@ -38,9 +38,10 @@ def json_vers_nx(chemin):
         for acteur in film['cast']:
             if acteur != act1:
                 G.add_edge(act1,acteur)
+    plt.clf()
+    nx.draw(G,with_labels=True)
+    plt.show()
     return G
-#json_vers_nx('dataSimplifiee.txt')
-
 G = json_vers_nx('dataSimplifiee.txt')
 
 
@@ -106,11 +107,38 @@ def est_proche(G,u,k):
 #print(collabProch(G,'Salim Kumar', 'Mohanlal'))
 
 #Q4
-def centralite(G,acteur):
-    distanceMax = collabProch(acteur,20)[-1][1]
-    return distanceMax
-#print(centralite(G,'Salim Kumar'))
+
+def centralite(G,u):
+    if u not in G.nodes:
+        print(u,"est un illustre inconnu")
+        return None
+    return G.degree(u)
+#print(centralite(G,'Mohanlal'))
+
+def centre_hollywood(G):
+    max = 0
+    act = ""
+    for acteur in G.nodes:
+        if max < G.degree(acteur):
+            max = G.degree(acteur)
+            act = acteur
+    return max,act 
+    #return nx.center(G)
+#print(centre_hollywood(G))
 
 #Q5
 def eloignement_max(G):
-    return ""
+    distances = dict(nx.all_pairs_shortest_path_length(G))
+    max_distance = 0
+    max_couple = None
+
+    for sommet1 in distances:
+        for sommet2, distance in distances[sommet1].items():
+            if distance > max_distance:
+                max_distance = distance
+                max_couple = (sommet1, sommet2)
+
+    return max_couple, max_distance
+
+print(eloignement_max(G))
+
